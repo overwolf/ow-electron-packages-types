45bb3e4dfdb08ce0aa5b62172eb5e082cd498aec
EladBahar <eladbahar@gmail.com>
2023-07-30 17:54:24 +0300

    Update game-list.d.ts

--- game-list.d.ts
+++ game-list.d.ts
@@ -1,8 +1,8 @@
 // game list version: 8963284
 export const enum kGameIds {
   DiabloIV = 22700,
-  GuildWars = 1136,
   LeagueofLegends = 5426,
+  LeagueofLegendsLauncher = 10902,
   LeagueofLegendsPBE = 22848,
   RocketLeague = 10798,
   TeamfightTactics = 21570,
