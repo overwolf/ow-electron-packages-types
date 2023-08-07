8b7489a3f3d3bdc716f2290a3ad6bb92f0190f99
EladBahar <eladbahar@gmail.com>
2023-08-07 16:46:40 +0300

    add EuroTruckSimulator2 & AmericanTruckSimulator

--- game-list.d.ts
+++ game-list.d.ts
@@ -1,8 +1,9 @@
 // game list version: 8963284
 export const enum kGameIds {
+  AmericanTruckSimulator = 10874,
   DiabloIV = 22700,
+  EuroTruckSimulator2 = 8584,
   LeagueofLegends = 5426,
-  LeagueofLegendsLauncher = 10902,
   LeagueofLegendsPBE = 22848,
   RocketLeague = 10798,
   TeamfightTactics = 21570,
