9e126bf4d60f159f62cf2bf8e21394b34b99bb8b
OdedItkinOW <oded.itkin@overwolf.com>
2023-11-27 09:54:06 +0200

    New supported games

--- game-list.ts
+++ game-list.ts
@@ -1,12 +1,14 @@
-// game list version: 8963284
 export const enum kGameIds {
   AmericanTruckSimulator = 10874,
   DiabloIV = 22700,
   EuroTruckSimulator2 = 8584,
   LeagueofLegends = 5426,
   LeagueofLegendsPBE = 22848,
+  Minecraft = 8032,
+  PathofExile = 7212,
   RocketLeague = 10798,
   TeamfightTactics = 21570,
+  VALORANT = 21640,
 }
 
 export const enum kLaunchersIds {
