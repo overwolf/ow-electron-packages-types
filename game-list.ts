c17e1ea82213f9071eddee2b0216966d86b41429
OdedItkinOW <oded.itkin@overwolf.com>
2024-01-14 12:46:16 +0200

    Gamelist 3

--- game-list.ts
+++ game-list.ts
@@ -1,19 +1,35 @@
+// game list version: 3
 export const enum kGameIds {
   AmericanTruckSimulator = 10874,
-  ARKSurvivalEvolved = 10778,
+  ApexLegends = 21566,
   ARKSurvivalAscended = 23476,
+  ARKSurvivalEvolved = 10778,
+  CS2 = 22730,
   DiabloIV = 22700,
+  Dota2 = 7314,
+  ELDENRING = 21866,
   EuroTruckSimulator2 = 8584,
   Fortnite = 21216,
+  GTAV = 1076,
   LeagueofLegends = 5426,
   LeagueofLegendsPBE = 22848,
+  LostArk = 21864,
   Minecraft = 8032,
+  MinecraftBedrock = 22176,
+  Osu = 3683,
+  Overwatch = 10844,
   PathofExile = 7212,
+  PUBG = 10906,
+  Rainbow6Siege = 10826,
   RocketLeague = 10798,
+  Rust = 10156,
   TeamfightTactics = 21570,
+  Valheim = 21668,
   VALORANT = 21640,
+  WorldofWarcraft = 765,
+  WoWClassic = 21630,
 }
 
 export const enum kLaunchersIds {
-  LeagueofLegendsLauncher = 10902,
+  LOLLauncher = 10902,
 }
