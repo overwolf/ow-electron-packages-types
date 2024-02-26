d96739ea557c20939fd55b75e3d4329f6f76592e
OdedItkinOW <oded.itkin@overwolf.com>
2024-02-26 10:46:42 +0200

    Updated supported gep games

--- gep-supported-games.ts
+++ gep-supported-games.ts
@@ -1,5 +1,10 @@
 export const enum kGepSupportedGameIds {
   DiabloIV = 22700,
   TeamfightTactics = 21570,
-  RocketLeague = 10798
+  RocketLeague = 10798,
+  Fortnite = 21216,
+  PathofExile = 7212,
+  VALORANT = 21640,
+  Dota2 = 7314,
+  CS2 = 22730,
 }
