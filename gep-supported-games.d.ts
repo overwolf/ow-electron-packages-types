127c0364dd1e9a41b06f6cbb4977920ad382859b
elad bahar <elad.bahar@overwolf.com>
2024-07-29 18:08:41 +0300

    update game-list.d.ts and gep-supported-games.d.ts (FFIV + WOW)

--- gep-supported-games.d.ts
+++ gep-supported-games.d.ts
@@ -3,6 +3,6 @@ export const enum kGepSupportedGameIds {
   RocketLeague = 10798,
   LeagueofLegends = 5426,
   LeagueofLegendsPBE = 22848,
-  WoWClassic = 21630,
+  WoW = 765,
   FinalFantasyXIVOnline = 6350,
 }
