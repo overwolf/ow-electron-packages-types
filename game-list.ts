c0a467dbf546656f44b0af352d9491efb23ad5d3
elad bahar <elad.bahar@overwolf.com>
2024-07-29 18:11:58 +0300

    update gep-supported-games and game-list

--- game-list.ts
+++ game-list.ts
@@ -1,28 +1,34 @@
-// game list version: 3
+// game list version: 6
+
+export const enum kLaunchersIds {
+  LOLLauncher = 10902,
+}
+
+// Overlay support games id's
 export const enum kGameIds {
   AmericanTruckSimulator = 10874,
   ApexLegends = 21566,
   ARKSurvivalAscended = 23476,
   ARKSurvivalEvolved = 10778,
-  CS2 = 22730,
   DiabloIV = 22700,
   Dota2 = 7314,
   ELDENRING = 21866,
-  EuroTruckSimulator2 = 8584,
+  Enshrouded = 23946,
+  EuroTruckSim2 = 8584,
+  FinalFantasyXIVOnlineARealmReborn = 6350,
   Fortnite = 21216,
   GTAV = 1076,
   LeagueofLegends = 5426,
   LeagueofLegendsPBE = 22848,
   LostArk = 21864,
   Minecraft = 8032,
-  MinecraftBedrock = 22176,
   Osu = 3683,
   Overwatch = 10844,
+  Palworld = 23944,
   PathofExile = 7212,
   PUBG = 10906,
   Rainbow6Siege = 10826,
   RocketLeague = 10798,
-  Rust = 10156,
   TeamfightTactics = 21570,
   Valheim = 21668,
   VALORANT = 21640,
@@ -30,6 +36,21 @@ export const enum kGameIds {
   WoWClassic = 21630,
 }
 
-export const enum kLaunchersIds {
-  LOLLauncher = 10902,
+// Detectable games without overlay support
+export const enum kNoOverlayGameIds {
+  BaldursGateIII = 22088,
+  CallOfDutyModernWarfareIII = 23424,
+  CS2 = 22730,
+  EscapeFromTarkov = 21634,
+  GenshinImpact = 21656,
+  HearthstoneHeroesofWarcraft = 9898,
+  HELLDIVERS2 = 24000,
+  LegendsofRuneterra = 21620,
+  LethalCompany = 23522,
+  MinecraftBedrock = 22176,
+  NewWorld = 21816,
+  Roblox = 4688,
+  Rust = 10156,
+  TheSims4 = 10540,
+  Warframe = 8954,
 }
--- gep-supported-games.ts
+++ gep-supported-games.ts
@@ -1,4 +1,8 @@
 export const enum kGepSupportedGameIds {
   TeamfightTactics = 21570,
-  RocketLeague = 10798
+  RocketLeague = 10798,
+  LeagueofLegends = 5426,
+  LeagueofLegendsPBE = 22848,
+  WoWClassic = 21630,
+  FinalFantasyXIVOnline = 6350,
 }
