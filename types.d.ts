d4d706ce11318b9ff85716c24b189d51ae691e7c
Edo Mathias <edo.mathias@gmail.com>
2025-06-25 22:40:00 +0300

    0.2.0-beta.3
    - Added `IOverwolfCRNApi`
    - Added `IOverwolfCRNApi` interface with methods and events.
    - Added `CRNActionTypes` for the `notification-action` event.

--- types.d.ts
+++ types.d.ts
@@ -96,6 +96,7 @@ interface OWPackages extends overwolf.packages.OverwolfPackageManager {
   recorder: IOverwolfRecordingApi;
   overlay: IOverwolfOverlayApi;
   utility: IOverwolfUtilityApi;
+  crn: IOverwolfCRNApi;
 }
 
 /**
@@ -1788,3 +1789,98 @@ interface IOverwolfRecordingApi {
    */
   on(eventName: 'stats', listener: (args: RecorderStats) => void): this;
 }
+
+// -----------------------------------------------------------------------------
+// Overwolf CRN API
+
+export interface ICRNEvent {
+  abort: () => void;
+}
+
+/**
+ * All supported notification actions.
+ */
+export type CRNActionType =
+  /**
+   * User clicked on the `X` button to dismiss the notification.
+   */
+  | 'Dismissed'
+  /**
+   * Notification closed due to launching another game while the notification is displayed.
+   */
+  | 'IgnoredByLaunchingGame'
+  /**
+   * Notification automatically closed after no user action was registered.
+   */
+  | 'Timeout'
+  /**
+   * User clicked on `Turn off notifications` from the cogwheel icon.
+   */
+  | 'TurnOffNotificationsRequested'
+  /**
+   * User clicked on a notification that opens an external URL (e.g., newsletter site).
+   */
+  | 'OpenExternalUrl'
+  /**
+   * User clicked on a notification that downloads an external app.
+   */
+  | 'DownloadExternalApp'
+  /**
+   * User clicked on the `Cancel` button while the external app was downloading.
+   */
+  | 'CancelDownloadExternalApp'
+  /**
+   * User clicked on the `X` button while the external app was downloading.
+   */
+  | 'CloseClickedWhileDownloadingExternalApp'
+  /**
+   * Developer programmatically closed the notification using the `closeNotificationWindow()` method.
+   */
+  | 'ForceClosed';
+
+export interface IOverwolfCRNApi {
+  /**
+   * returns Notification visible status.
+   * true if the notification is currently visible, false if not.
+   */
+  isNotificationVisible(): Promise<boolean>;
+
+  /**
+   * returns Notification settings status:
+   * true if notifications are allowed, false if not.
+   */
+  getNotificationStatus(): Promise<boolean>;
+
+  /**
+   * Closes the notification.
+   */
+  closeNotificationWindow(): void;
+
+  /**
+   * Enables or disables notifications.
+   * @param enable - true to enable notifications, false to disable.
+   */
+  allowNotifications(enable: boolean): void;
+
+  /**
+   * Fired before a notification is shown.
+   * @param eventName
+   * @param listener
+   */
+  on(
+    eventName: 'before-notification',
+    listener: (event: ICRNEvent, args: any) => void
+  ): this;
+
+  /**
+   * Fired when a notification action is triggered.
+   * @param eventName
+   * @param listener
+   */
+  on(
+    eventName: 'notification-action',
+    listener: (event: CRNActionType) => void
+  ): this;
+}
+
+// -----------------------------------------------------------------------------
